d9b5af942482cb2cf51036159672bcf00e1ad649
Vlad Vlasenko <43779827+VasiL1X@users.noreply.github.com>
2020-06-12 19:40:12 +0300

    Update clickOutside.js

--- clickOutside.js
+++ clickOutside.js
@@ -1,13 +1,13 @@
 export default {
   bind: function (el, binding, vnode) {
-    this.event = function (event) {
+      el.clickOutsideEvent = function (event) {
       if (!(el == event.target || el.contains(event.target))) {
         vnode.context[binding.expression](event);
       }
     };
-    document.body.addEventListener('click', this.event)
+    document.body.addEventListener('click', el.clickOutsideEvent)
   },
   unbind: function (el) {
-    document.body.removeEventListener('click', this.event)
+    document.body.removeEventListener('click', el.clickOutsideEvent)
   },
 }
